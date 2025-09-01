c3d77fa5d16ace16d5d2d8cd1f1ffe4fdd7c3701
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-01 07:37:03 -0500

    feat(resource): add resource implementation.

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -1,5 +1,29 @@
-//
-// Created by Alumnos on 27/8/25.
-//
-
 #include "Resource.h"
+
+/***
+ * @brief Constructor for the Resource class.
+ *
+ * @param name The name of the resource.
+ * @param type The type of the resource (Consumable or Usable).
+ */
+Resource::Resource(std::string name, const Type type)
+    : name(std::move(name)), isAvailable(true), resourceType(type) {}
+
+/**
+ * @brief Retrieves the name of the resource.
+ * @return The name of the resource.
+ */
+std::string Resource::getName() const {
+    return name;
+}
+
+/**
+ * @brief Retrieves the type of the resource.
+ * @return The type of the resource (Consumable or Usable).
+ */
+Resource::Type Resource::getResourceType() const {
+    return resourceType;
+}
+
+
+

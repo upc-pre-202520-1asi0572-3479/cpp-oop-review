ffe7a4670b51e2207d57e64a3df18b832436d9f9
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-01 09:56:14 -0500

    feat(main): add main behavior.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#include "Process.h"
+#include "Task.h"
+#include "UsableResource.h"
+#include "ConsumableResource.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    const auto compilationProcess = std::unique_ptr<Process>(
+new Process("Compilation Process", "Compilation Process description",
+    { "CentralProcessingUnit", "Memory"}, 15));
+    compilationProcess->addResource(
+        std::unique_ptr<UsableResource>(new UsableResource("CentralProcessingUnit", 4)));
+    compilationProcess->addResource(std::unique_ptr<ConsumableResource>(new ConsumableResource("Memory", 4096)));
+
 }
